2512f72d553137ef002028a66687783163588538
HoangB21 <tonghoang213@gmail.com>
2025-10-06 14:17:37 +0700

    dev branch

--- src/components/stories/Stories.jsx
+++ src/components/stories/Stories.jsx
@@ -18,15 +18,15 @@ const Stories = () => {
   return (
     <div className="stories">
       <div className="story">
-        <img src={"/upload/" + currentUser.profilePic} alt="" />
+        <img src={currentUser.profilePic} alt="" />
         <span>{currentUser.name}</span>
         <button>+</button>
       </div>
       {error
         ? "Something went wrong"
         : isLoading
-        ? "loading"
-        : data.map((story) => (
+          ? "loading"
+          : data.map((story) => (
             <div className="story" key={story.id}>
               <img src={story.img} alt="" />
               <span>{story.name}</span>
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -20,7 +20,7 @@ const Register = () => {
     e.preventDefault();
 
     try {
-      await axios.post("http://3.107.222.139:8800/api/auth/register", inputs);
+      await axios.post("http://localhost:8800/api/auth/register", inputs);
     } catch (err) {
       setErr(err.response.data);
     }
