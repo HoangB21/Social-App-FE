049c820a7b7f1eb10b6f409f941de5c8e899508c
HoangB21 <tonghoang213@gmail.com>
2025-10-06 11:23:05 +0700

    change backend url

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -9,7 +9,7 @@ export const AuthContextProvider = ({ children }) => {
   );
 
   const login = async (inputs) => {
-    const res = await axios.post("http://localhost:8800/api/auth/login", inputs, {
+    const res = await axios.post("http://3.107.222.139:8800/api/auth/login", inputs, {
       withCredentials: true,
     });
 
