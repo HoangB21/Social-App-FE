98bdc51f32fe7e1dcf3c0101433cacf22884a55c
HoangB21 <tonghoang213@gmail.com>
2025-10-07 12:26:46 +0700

    add interceptors for axios

--- src/axios.js
+++ src/axios.js
@@ -4,3 +4,14 @@ export const makeRequest = axios.create({
   baseURL: "http://52.64.14.178:8800/api/",
   withCredentials: true,
 });
+
+makeRequest.interceptors.request.use((config) => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const token = user?.accessToken;
+  console.log("Axios Interceptor - Token:", token);
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
