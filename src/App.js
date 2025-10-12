c8d3f6c05eaebdcfbbc79f1704dda3512223e4a9
HoangB21 <tonghoang213@gmail.com>
2025-10-12 18:36:43 +0700

    add right bar and fix image size

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import {
 } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import LeftBar from "./components/leftBar/LeftBar";
-// import RightBar from "./components/rightBar/RightBar";
+import RightBar from "./components/rightBar/RightBar";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 import "./style.scss";
@@ -34,7 +34,7 @@ function App() {
             <div style={{ flex: 6 }}>
               <Outlet />
             </div>
-            {/* <RightBar /> */}
+            <RightBar />
           </div>
         </div>
       </QueryClientProvider>
